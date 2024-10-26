a07af40033e53cc0de007e2a59c23efce2883021
Katerina Kurochkina <katifedr@mail.ru>
2024-10-26 15:38:59 +0500

    home, contacts

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 
 
@@ -8,4 +8,16 @@ def home(request):
 
 
 def contacts(request):
+    return render(request, 'catalog/contacts.html')
+
+
+def contact_data(request):
+    if request.method == 'POST':
+        # Получение данных из формы
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        # Обработка данных (например, сохранение в БД, отправка email и т. д.)
+        # Здесь мы просто возвращаем простой ответ
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
     return render(request, 'catalog/contacts.html')
